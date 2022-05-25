ac61951e20bc505a8941bc5601f8722cbeb781a9
conno <conno@10.0.0.199>
2022-05-25 16:05:20 -0600

    from home

--- GameOfLife/src/GameOfLife.java
+++ GameOfLife/src/GameOfLife.java
@@ -1,32 +1,83 @@
 
 public class GameOfLife {
-
-	public GameOfLife(int i, int j) {
-		// TODO Auto-generated constructor stub
+	boolean[][] board;
+	int columns;
+	int rows;
+	int neighbours;
+	
+	public GameOfLife(int rows, int columns) {
+		
+		if (rows > 0 && columns > 0) 
+		{
+			this.columns = columns;
+			this.rows = rows;
+		}
 	}
 
 	public Object getColumns() {
-		// TODO Auto-generated method stub
-		return null;
+		return columns;
 	}
 
 	public Object getRows() {
-		// TODO Auto-generated method stub
-		return null;
+		return rows;
 	}
 
-	public Object isAlive(int i, int j) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public Object getNeighbourCount(int r, int c) {
+		int count = 0;
+//		int i, j;
+//		for(i = r - 1; i <= r + 1; i++){
+//	        for(j = c - 1; j <= c + 1; j++){
+//	            if((i == r && j == c) || (i < 0 || j < 0) || (i >= rows || j >= columns)){
+//	                continue;
+//	            }
+//	            if(board [i][j] == true){
+//	                count++;
+//	                neighbours += count;
+//	            }
+//	        }
+//	    }
+		for (int i = 0; i <= r; i++) {
+			for (int j = 0; j <= c; j++) {
+				if (board[i][j] == true) 
+				{
+					count += 1;
+					neighbours += count;
+				}
+				else 
+				{
+					neighbours += count;
+				}
 
-	public Object getNeighbourCount(int i, int j) {
-		// TODO Auto-generated method stub
-		return null;
+
+			}
+		}
+		return count;
+	} 
+	
+	public boolean isAlive(int i, int j) {
+		boolean isAlive = false;
+		if (neighbours > 3 || neighbours < 2) {
+			isAlive = false;
+		}
+		else 
+		{
+		isAlive = true;
+		}
+		return isAlive;
 	}
 
 	public void calculateNextGeneration() {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (isAlive(i, j) == true) {
+					board[i][j] = true;
+				}
+				else
+				{
+					board[i][j] = false;
+				}
+			}
+		}
 		
 	}
 
