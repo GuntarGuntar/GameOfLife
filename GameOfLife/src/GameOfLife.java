44e7de9a4107bc30352e53680ea62ebed4a7347b
conno <conno@10.0.0.199>
2022-05-25 11:14:53 -0600

    file made

---
+++ GameOfLife/src/GameOfLife.java
@@ -0,0 +1,38 @@
+
+public class GameOfLife {
+
+	public GameOfLife(int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object getColumns() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getRows() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object isAlive(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getNeighbourCount(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void calculateNextGeneration() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAlive(int i, int j, boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
