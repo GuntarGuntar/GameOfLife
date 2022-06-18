c10a41297844f9c68d081508ca764fd3129bc0d9
conno <conno@10.0.0.199>
2022-06-17 21:02:15 -0600

    not done:(

--- GameOfLife/src/GameOfLife.java
+++ GameOfLife/src/GameOfLife.java
@@ -1,6 +1,7 @@
 
 public class GameOfLife {
-	private boolean[][] board;
+	boolean[][] board;
+	boolean[][] newGen;
 	int columns;
 	int rows;
 	int neighbours;
@@ -11,6 +12,8 @@ public class GameOfLife {
 		{
 			this.columns = columns;
 			this.rows = rows;
+			board = new boolean[rows][columns];
+			newGen = new boolean[rows][columns];
 		}
 		else 
 		{
@@ -26,32 +29,50 @@ public class GameOfLife {
 		return rows;
 	}
 
-	public int getNeighbourCount(int r, int c) {
-		int count = 0;
-		for (int i = rows - 1; i <= rows + 1; i++) {
-			for (int j = columns - 1; j <= columns; j++) {
-				if (!(i == rows && j == columns)) {
-					if (board[i][j] = true) 
-					{
-						count++;
-					}
-					else 
-					{
+	public int getNeighbourCount(int y, int x) {
+		int neighbourCount = 0;
+		
+		for (int i = y - 1; i < y + 2; i++) {
+			for (int j = x - 1; j < x + 2; j++) {
+				if (i == y && j == x) {}
+				else {		
+					try {
+						if (board[i][j] == true) {
+							neighbourCount += 1;
+						}
+					} catch (Exception exception) {
+						int newI = i;
+						int newJ = j;
+						if (i >= board.length) {
+							newI -= board.length;
+						}
+						else if (i < 0) {
+							newI += board.length;
+						}
+						if (j >= board[newI].length) {
+							newJ -= board[newI].length;
+						}
+						else if (j < 0) {
+							newJ += board[newI].length;
+						}
+						if (board[newI][newJ] == true) {
+							neighbourCount += 1;
+						}
 					}
 				}
 			}
 		}
-		return count;
+		
+		return neighbourCount;
 	}
-	
+
 	public boolean isAlive(int i, int j) {
 		boolean isAlive = false;
-		if (neighbours > 3 || neighbours < 2) {
-			isAlive = false;
+		if (i > 0 && j > 0 && i < board.length && j < board.length) {
+			isAlive = true;
 		}
-		else 
-		{
-		isAlive = true;
+		else {
+			isAlive = false;
 		}
 		return isAlive;
 	}
@@ -59,22 +80,42 @@ public class GameOfLife {
 	public void calculateNextGeneration() {
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < columns; j++) {
-				if (isAlive(i, j) == true) {
-					board[i][j] = true;
+				if (getNeighbourCount(i, j) >= 4) {
+					newGen[i][j] = false;
 				}
-				else
-				{
-					board[i][j] = false;
+				else if(getNeighbourCount(i, j) < 4 && getNeighbourCount(i, j) > 1){
+					newGen[i][j] = true;
 				}
+				else if (isAlive(i, j) == false && getNeighbourCount(i, j) == 3) {
+					newGen[i][j] = true;
+				}
+				else {
+					newGen[i][j] = false;
+				}
+				board[i][j] = newGen[i][j];
 			}
 		}
-		
 	}
 
 	public void setAlive(int i, int j, boolean b) {
-		// TODO Auto-generated method stub
-		
+		try {
+			board[i][j] = b;
+		} catch (Exception e) {
+		}
 	}
-
+	public String toString() {
+		String output = "";
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (board[i][j] == true) {
+					output += "*";
+				}
+				else {
+					output += " ";
+				}
+				output += "\r\n";
+			}
+		}
+		return output;
+	}
 }
-
